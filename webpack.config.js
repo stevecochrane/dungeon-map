bbc54d9c6203768ec36a19119b9cb8cdd64ff596
Steve Cochrane <steve@stevecochrane.com>
2017-08-27 16:51:53 -0500

    Added babel-loader support to Webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
 	output: {
 		filename: "bundle.js",
 		path: path.join(__dirname, "/public/js")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/i,
+				loader: "babel-loader"
+			}
+		]
 	}
 };
