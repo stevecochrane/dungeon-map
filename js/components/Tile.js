0bce2c12b2c26114dd2773eb7c8d56e91aba9a95
Steve Cochrane <steve@stevecochrane.com>
2018-05-12 16:14:09 -0500

    Add ability to toggle isActive class on click for Tile

--- js/components/Tile.js
+++ js/components/Tile.js
@@ -3,11 +3,30 @@ import React from "react";
 class Tile extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			isActive: false
+		};
 	}
 	render() {
 		return(
-			<div>Tile</div>
+			<div
+				className={this.state.isActive ? "is-active" : ""}
+				onClick={() => { this._toggleTile(); }}>
+				Tile
+			</div>
 		);
+	}
+	_toggleTile() {
+		if (this.state.isActive) {
+			this.setState({
+				isActive: false
+			});
+		} else {
+			this.setState({
+				isActive: true
+			});
+		}
 	}
 }
 
