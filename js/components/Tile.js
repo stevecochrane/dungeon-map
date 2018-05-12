dde55cdfa070bd964fac34413a966843e631b0c5
Steve Cochrane <steve@stevecochrane.com>
2018-05-12 15:54:00 -0500

    Start new Tile component

---
+++ js/components/Tile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Tile extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return(
+			<div>Tile</div>
+		);
+	}
+}
+
+export default Tile;
--- js/components/app.js
+++ js/components/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import EditableTextField from "./EditableTextField";
+import Tile from "./Tile";
 
 class App extends React.Component {
 	render() {
@@ -7,6 +8,7 @@ class App extends React.Component {
 			<div className="app">
 				<h1>DUNGEON MAP</h1>
 				<EditableTextField />
+				<Tile />
 			</div>
 		);
 	}
