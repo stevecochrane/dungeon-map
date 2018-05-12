75b0553246cb3537171ca248bc8698d9f272b001
Steve Cochrane <steve@stevecochrane.com>
2018-05-12 16:58:10 -0500

    Add a test CSS file to the app

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
+import "../css/main.css";
+
 document.addEventListener("DOMContentLoaded", function() {
 	ReactDOM.render(<App />, document.getElementById("app"));
 });
