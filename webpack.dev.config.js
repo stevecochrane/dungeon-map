1b694cdca8c170b0691bd6c7aef6276bc74c3188
Steve Cochrane <steve@stevecochrane.com>
2018-05-12 22:43:44 -0500

    Split Webpack config into separate files for dev and prod

---
+++ webpack.dev.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./app/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.join(__dirname, "/public/js")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				enforce: "pre",
+				loader: "postcss-loader",
+				options: {
+					plugins: () => {[
+						require("stylelint")()
+					]}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					{
+						loader: "postcss-loader",
+						options: {
+							plugins: () => ([
+								require("autoprefixer")()
+							])
+						}
+					}
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				enforce: "pre",
+				loader: "eslint-loader",
+				options: {
+					emitWarning: true
+				}
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: {
+					cacheDirectory: true
+				}
+			}
+		]
+	}
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+	mode: "production",
 	entry: "./app/index.js",
 	output: {
 		filename: "bundle.js",
