7b6cb9b577bf23a467b94d5a89a63b7426363435
Steve Cochrane <steve@stevecochrane.com>
2019-03-07 22:13:01 -0600

    Add ToolPalette component

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Map from "../Map/Map";
+import ToolPalette from "../ToolPalette/ToolPalette";
 
 class App extends Component {
 	render() {
@@ -7,6 +8,7 @@ class App extends Component {
 			<div className="flex flex-col flex-grow">
 				<h1 className="py-8 text-center">Dungeon Map</h1>
 				<Map rows={10} columns={10} className="flex flex-grow flex-col justify-center" />
+				<ToolPalette />
 			</div>
 		);
 	}
---
+++ app/components/ToolPalette/ToolPalette.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+// import "./ToolPalette.css";
+
+class ToolPalette extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div className="ToolPalette absolute pin-r pin-b"></div>
+		);
+	}
+
+}
+
+// ToolPalette.propTypes = {};
+// ToolPalette.defaultProps = {};
+
+export default ToolPalette;
