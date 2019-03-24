93f147f082750ac0cfd01ccffa488c234f26c629
Steve Cochrane <steve@stevecochrane.com>
2019-03-24 10:13:51 -0500

    Enable Redux DevTools

--- app/index.js
+++ app/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 import App from "./components/App/App";
 
 import "./styles/main.css";
 
-const store = createStore(reducers);
+const store = createStore(reducers, composeWithDevTools());
 
 document.addEventListener("DOMContentLoaded", () => {
 	const rootElem = document.getElementById("app");
