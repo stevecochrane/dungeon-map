98f06fdda159fed7099cc88ebab9d929583b6375
Steve Cochrane <steve@stevecochrane.com>
2019-03-24 10:49:02 -0500

    Convert App to stateless functional component

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Map from "../Map/Map";
 import ToolPalette from "../ToolPalette/ToolPalette";
 
-class App extends Component {
-	render() {
-		return (
-			<div className="flex flex-col flex-grow">
-				<h1 className="py-8 text-center">Dungeon Map</h1>
-				<Map rows={10} columns={10} className="flex flex-grow flex-col justify-center" />
-				<ToolPalette />
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div className="flex flex-col flex-grow">
+			<h1 className="py-8 text-center">Dungeon Map</h1>
+			<Map rows={10} columns={10} className="flex flex-grow flex-col justify-center" />
+			<ToolPalette />
+		</div>
+	);
+};
 
 export default App;
