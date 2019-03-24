5835b72c5340e9bb84c064c3b3125b331470feb0
Steve Cochrane <steve@stevecochrane.com>
2019-03-24 09:40:38 -0500

    Add Provider and empty store

--- app/index.js
+++ app/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./components/App/App";
 
 import "./styles/main.css";
 
-document.addEventListener("DOMContentLoaded", function() {
-	ReactDOM.render(<App />, document.getElementById("app"));
+const store = createStore(function() {});
+
+document.addEventListener("DOMContentLoaded", () => {
+	const rootElem = document.getElementById("app");
+
+	ReactDOM.render(
+		<Provider store={store}>
+			<App store={store} />
+		</Provider>,
+		rootElem
+	);
 });
