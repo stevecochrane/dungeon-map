32465d166dca7d5cd3371c131364f3e54844da4a
Steve Cochrane <steve@stevecochrane.com>
2019-03-24 10:22:33 -0500

    Convert ToolPalette to stateless functional component

--- app/components/ToolPalette/ToolPalette.js
+++ app/components/ToolPalette/ToolPalette.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 // import PropTypes from "prop-types";
 import Tool from "../Tool/Tool";
 import "./ToolPalette.css";
 
-class ToolPalette extends Component {
-
-	render() {
-		return(
-			<div className="ToolPalette absolute pin-r pin-b">
-				<Tool />
-				<Tool />
-			</div>
-		);
-	}
-
-}
+const ToolPalette = () => {
+	return(
+		<div className="ToolPalette absolute pin-r pin-b">
+			<Tool />
+			<Tool />
+		</div>
+	);
+};
 
 // ToolPalette.propTypes = {};
 // ToolPalette.defaultProps = {};
