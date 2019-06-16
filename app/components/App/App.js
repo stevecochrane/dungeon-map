d289d678e803c3fa00fe8602bf2f82e2a0427155
Steve Cochrane <steve@stevecochrane.com>
2019-06-15 20:42:53 -0500

    Adjust for Tailwind 1.0 header style changes

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -6,7 +6,7 @@ import ToolPalette from "../ToolPalette/ToolPalette";
 const App = ({activeTool, onToolClick}) => {
 	return (
 		<div className="flex flex-col flex-grow">
-			<h1 className="py-8 text-center">Dungeon Map</h1>
+			<h1 className="font-bold py-8 text-3xl text-center">Dungeon Map</h1>
 			<Map rows={10} columns={10} className="flex flex-grow flex-col justify-center" />
 			<ToolPalette activeTool={activeTool} onToolClick={onToolClick} />
 		</div>
