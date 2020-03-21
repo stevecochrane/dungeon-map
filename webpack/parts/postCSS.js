035a584b629ed67272cad69165feba750457322c
Steve Cochrane <steve@stevecochrane.com>
2020-03-21 14:26:48 -0500

    Add postcss-purgecss to cut unused Tailwind classes

--- webpack/parts/postCSS.js
+++ webpack/parts/postCSS.js
@@ -1,9 +1,16 @@
 const postcssPresetEnv = require("postcss-preset-env");
+const postcssPurgeCss = require("@fullhuman/postcss-purgecss");
 const tailwindcss = require("tailwindcss");
 
 module.exports = () => ({
   loader: "postcss-loader",
   options: {
-    plugins: () => [tailwindcss(), postcssPresetEnv()]
+    plugins: () => [
+      tailwindcss(),
+      postcssPresetEnv(),
+      postcssPurgeCss({
+        content: ["./app/templates/**/*.html", "./app/components/**/*.js"]
+      })
+    ]
   }
 });
