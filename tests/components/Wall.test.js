321912a82af906690b289fd4cb93f62a21fad506
Steve Cochrane <steve@stevecochrane.com>
2020-04-11 10:30:04 -0500

    Add test for clicking Wall to toggle active state

--- tests/components/Wall.test.js
+++ tests/components/Wall.test.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Wall from "../../app/components/Wall";
 
 describe("Wall", () => {
   test("Renders with no props", () => {
     const { getByTestId } = render(<Wall />);
     expect(getByTestId("Wall")).toBeInTheDocument();
+  });
+
+  test("Clicking Wall toggles active state", () => {
+    const { getByTestId } = render(<Wall />);
+    const wall = getByTestId("Wall");
+    expect(wall.getAttribute("data-active")).toBe("false");
+    fireEvent.click(wall);
+    expect(wall.getAttribute("data-active")).toBe("true");
+    fireEvent.click(wall);
+    expect(wall.getAttribute("data-active")).toBe("false");
   });
 });
