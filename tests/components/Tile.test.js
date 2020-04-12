d0bfb165e8d91badc62bd341c672361452af070f
Steve Cochrane <steve@stevecochrane.com>
2020-04-12 10:12:00 -0500

    Add test for clicking Tile surface to toggle active state

--- tests/components/Tile.test.js
+++ tests/components/Tile.test.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Tile from "../../app/components/Tile";
 
 describe("Tile", () => {
-
   test("Renders with no props", () => {
     const { getByTestId } = render(<Tile />);
     expect(getByTestId("Tile")).toBeInTheDocument();
   });
 
+  test("Clicking Tile surface toggles active state", () => {
+    const { getByTestId } = render(<Tile />);
+    const tileSurface = getByTestId("Tile-surface");
+    expect(tileSurface.getAttribute("data-active")).toBe("false");
+    fireEvent.click(tileSurface);
+    expect(tileSurface.getAttribute("data-active")).toBe("true");
+    fireEvent.click(tileSurface);
+    expect(tileSurface.getAttribute("data-active")).toBe("false");
+  });
 });
