5dd380990b8d6076ece7c561b1b64405cdee8555
Steve Cochrane <steve@stevecochrane.com>
2020-04-19 16:37:55 -0500

    Add tests for Tool displaying active state

--- tests/components/Tool.test.js
+++ tests/components/Tool.test.js
@@ -3,10 +3,18 @@ import { render } from "@testing-library/react";
 import Tool from "../../app/components/Tool";
 
 describe("Tool", () => {
-
   test("Renders with no props", () => {
     const { getByTestId } = render(<Tool />);
     expect(getByTestId("Tool")).toBeInTheDocument();
   });
 
+  test("Renders as inactive if not active tool", () => {
+    const { getByTestId } = render(<Tool id="foo" activeTool="bar" />);
+    expect(getByTestId("Tool").getAttribute("data-active")).toBe("false");
+  });
+
+  test("Renders as active if active tool", () => {
+    const { getByTestId } = render(<Tool id="foo" activeTool="foo" />);
+    expect(getByTestId("Tool").getAttribute("data-active")).toBe("true");
+  });
 });
