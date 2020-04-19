68e4f4f5e02f02a2dea12325bee06807a0bb35e2
Steve Cochrane <steve@stevecochrane.com>
2020-04-19 16:25:14 -0500

    Add active styles for Tool

--- app/components/Tool.js
+++ app/components/Tool.js
@@ -1,19 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Tool = ({id, name, activeTool, onToolClick}) => {
-  let className = "w-12 h-12 m-2 cursor-pointer bg-yellow-200 transition-bg hover:bg-yellow-400";
+const Tool = ({ id, name, activeTool, onToolClick }) => {
+  let className = "w-12 h-12 m-2 cursor-pointer transition-bg";
   if (id === activeTool) {
-    // TODO: active styles
+    className += " bg-yellow-400";
+  } else {
+    className += " bg-yellow-200 hover:bg-yellow-400";
   }
   return (
     <div
-        className={className}
-        data-id={id}
-        data-name={name}
-        data-testid="Tool"
-        onClick={() => onToolClick(id, name)}>
-    </div>
+      className={className}
+      data-id={id}
+      data-name={name}
+      data-testid="Tool"
+      onClick={() => onToolClick(id, name)}
+    ></div>
   );
 };
 
