037bfd1404820c9bdb2d9a824eb57a98594fa07f
Steve Cochrane <steve@stevecochrane.com>
2020-04-19 16:29:02 -0500

    Add data-active attribute to Tool for easier testing

--- app/components/Tool.js
+++ app/components/Tool.js
@@ -2,15 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Tool = ({ id, name, activeTool, onToolClick }) => {
+  const active = id === activeTool;
   let className = "w-12 h-12 m-2 cursor-pointer transition-bg";
-  if (id === activeTool) {
+
+  if (active) {
     className += " bg-yellow-400";
   } else {
     className += " bg-yellow-200 hover:bg-yellow-400";
   }
+
   return (
     <div
       className={className}
+      data-active={active}
       data-id={id}
       data-name={name}
       data-testid="Tool"
