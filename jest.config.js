99d112ccd9317ba7e98a5c4f30b7c587f71720b1
Steve Cochrane <steve@stevecochrane.com>
2020-05-25 14:13:03 -0500

    Set Jest collectCoverage to true

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
@@ -123,7 +123,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["./tests/setup.js"],
+  setupFilesAfterEnv: ["./tests/setup.js"]
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
