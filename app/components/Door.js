d1566b48ac1895cfef8f9e1a5492c8b8d0d49bf2
Steve Cochrane <steve@stevecochrane.com>
2020-08-02 22:09:24 -0500

    Add visible door shape for Door lines

--- app/components/Door.js
+++ app/components/Door.js
@@ -3,11 +3,18 @@ import PropTypes from "prop-types";
 import toolTypes from "../constants/toolTypes";
 
 const Door = ({ activeTool }) => {
-  let classes = "absolute bg-blue-600 inset-0 z-30";
+  let wallClasses = "absolute bg-blue-600 inset-0 z-30";
   if (activeTool === toolTypes.DOOR || activeTool === toolTypes.WALL) {
-    classes += " cursor-pointer duration-200 transition-colors hover:transition-none";
+    wallClasses += " cursor-pointer duration-200 transition-colors hover:transition-none";
   }
-  return <div className={classes} data-testid="Door"></div>;
+  const doorClasses =
+    "absolute bg-yellow-600 h-4 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2";
+
+  return (
+    <div className={wallClasses} data-testid="Door">
+      <div className={doorClasses}></div>
+    </div>
+  );
 };
 
 Door.propTypes = {
