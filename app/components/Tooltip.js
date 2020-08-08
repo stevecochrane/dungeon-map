f50cd37cfcf8134deeedf1c98e10cf294011fd85
Steve Cochrane <steve@stevecochrane.com>
2020-08-08 14:52:08 -0500

    Add placeholder text and styles to Tooltip

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -3,7 +3,9 @@ import React from "react";
 const Tooltip = () => {
   return (
     <div className="absolute inset-0" data-testid="Tooltip">
-      <div className="absolute bg-gray-800 h-16 left-1/2 rounded-md top-0 transform -translate-x-1/2 -translate-y-full w-32"></div>
+      <div className="absolute bg-gray-800 left-1/2 px-4 py-2 rounded-md text-center text-gray-200 top-0 transform -translate-x-1/2 -translate-y-full w-56">
+        {"Hello this is a message"}
+      </div>
     </div>
   );
 };
