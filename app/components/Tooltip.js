640dd4adfad3b8f842d50dc796eb4ef54032b634
Steve Cochrane <steve@stevecochrane.com>
2020-08-09 13:57:52 -0500

    Prevent hover on Tooltip itself

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -4,7 +4,7 @@ const Tooltip = () => {
   return (
     <div className="absolute group inset-0" data-testid="Tooltip">
       <div
-        className="absolute bg-gray-800 duration-300 left-1/2 opacity-0 px-4 py-2 rounded-md text-center text-gray-200 top-0 transform transition-all -translate-x-1/2 -translate-y-3/2 w-56 group-hover:opacity-100 group-hover:-translate-y-full group-hover:transition-none"
+        className="absolute bg-gray-800 duration-300 left-1/2 opacity-0 pointer-events-none px-4 py-2 rounded-md text-center text-gray-200 top-0 transform transition-all -translate-x-1/2 -translate-y-3/2 w-56 group-hover:opacity-100 group-hover:-translate-y-full group-hover:transition-none"
         style={{ marginTop: "-0.5rem" }}
       >
         {"Hello this is a message"}
