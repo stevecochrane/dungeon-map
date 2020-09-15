75cb4b22da09595f6407b50aaeeace64da92fd94
Steve Cochrane <steve@stevecochrane.com>
2020-09-14 20:15:28 -0500

    Invert use of gray

--- app/components/BlankCell.js
+++ app/components/BlankCell.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 import toolTypes from "../constants/toolTypes";
 
 const BlankCell = ({ activeTool }) => {
-  let classes = "absolute bg-gray-200 inset-0";
+  let classes = "absolute bg-white inset-0";
   if (activeTool === toolTypes.NOTE || activeTool === toolTypes.ROOM) {
     classes +=
-      " cursor-pointer duration-300 transition-colors hover:bg-gray-400 hover:transition-none";
+      " cursor-pointer duration-300 transition-colors hover:bg-gray-200 hover:transition-none";
   }
   return <div className={classes} data-testid="BlankCell"></div>;
 };
--- app/components/BlankLine.js
+++ app/components/BlankLine.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 import toolTypes from "../constants/toolTypes";
 
 const BlankLine = ({ activeTool }) => {
-  let classes = "absolute bg-white inset-0 z-10";
+  let classes = "absolute bg-gray-400 inset-0 z-10";
   if (activeTool === toolTypes.DOOR || activeTool === toolTypes.WALL) {
     classes +=
-      " duration-300 transition-colors group-hover:bg-gray-400 group-hover:transition-none group-hover:z-20";
+      " duration-300 transition-colors group-hover:bg-gray-600 group-hover:transition-none group-hover:z-20";
   }
   return <div className={classes} data-testid="BlankLine"></div>;
 };
