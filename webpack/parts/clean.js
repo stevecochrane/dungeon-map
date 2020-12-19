8786841b9a87e1ac659d8ab9d2b38b06e2f1b959
Steve Cochrane <steve@stevecochrane.com>
2020-12-18 21:57:43 -0600

    Delete all old Webpack configs

--- webpack/parts/clean.js
+++
@@ -1,9 +0,0 @@
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-
-module.exports = (path) => ({
-  plugins: [
-    new CleanWebpackPlugin([path], {
-      root: process.cwd()
-    })
-  ]
-});
