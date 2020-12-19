d5c41cd77c14c3ed14acf2947e9931e592262b3c
Steve Cochrane <steve@stevecochrane.com>
2020-12-19 15:41:02 -0600

    Re-alphabetize props where tw was added

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ import ToolPalette from "./ToolPalette";
 
 const App = ({ activeTool, isMouseDown, onToolClick }) => {
   return (
-    <div tw="flex flex-col flex-grow" data-testid="App">
+    <div data-testid="App" tw="flex flex-col flex-grow">
       {/* <h1 className="font-bold py-8 text-3xl text-center">Dungeon Map</h1> */}
       <Map
         activeTool={activeTool}
-        tw="flex flex-grow flex-col justify-center"
         columns={10}
         isMouseDown={isMouseDown}
         rows={10}
+        tw="flex flex-grow flex-col justify-center"
       />
       <ToolPalette activeTool={activeTool} onToolClick={onToolClick} />
     </div>
