978c7a6d578cd6503a728df0a32e23538075bbf1
Steve Cochrane <steve@stevecochrane.com>
2020-12-19 15:39:52 -0600

    Convert App to twin.macro

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "twin.macro";
 import Map from "./Map";
 import ToolPalette from "./ToolPalette";
 
 const App = ({ activeTool, isMouseDown, onToolClick }) => {
   return (
-    <div className="flex flex-col flex-grow" data-testid="App">
+    <div tw="flex flex-col flex-grow" data-testid="App">
       {/* <h1 className="font-bold py-8 text-3xl text-center">Dungeon Map</h1> */}
       <Map
         activeTool={activeTool}
-        className="flex flex-grow flex-col justify-center"
+        tw="flex flex-grow flex-col justify-center"
         columns={10}
         isMouseDown={isMouseDown}
         rows={10}
