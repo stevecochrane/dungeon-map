bc9acf2c0f97714aaa01aa0e4079d4e3f09d70ef
Steve Cochrane <steve@stevecochrane.com>
2020-12-19 15:13:29 -0600

    Rename .babelrc to babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-react"],
+    [
+      "@babel/preset-env",
+      {
+        corejs: {
+          proposals: true,
+          version: 3
+        },
+        useBuiltIns: "usage"
+      }
+    ]
+  ],
+  plugins: []
+};
