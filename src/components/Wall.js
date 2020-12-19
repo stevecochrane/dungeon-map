09341cf2808b31d24062c6a1dfa066793b984702
Steve Cochrane <steve@stevecochrane.com>
2020-12-19 15:41:02 -0600

    Re-alphabetize props where tw was added

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -2,7 +2,7 @@ import React from "react";
 import "twin.macro";
 
 const Wall = () => {
-  return <div tw="absolute bg-blue-600 -inset-halfLine z-30" data-testid="Wall"></div>;
+  return <div data-testid="Wall" tw="absolute bg-blue-600 -inset-halfLine z-30"></div>;
 };
 
 export default Wall;
