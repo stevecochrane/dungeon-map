804a34f61b85b1f91dd3ab88639cb6fea07fdcfe
Steve Cochrane <steve@stevecochrane.com>
2021-04-15 22:02:58 -0500

    Add a type for Room props

--- src/components/Room.ts
+++ src/components/Room.ts
@@ -1,17 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
 import toolTypes from "../constants/toolTypes";
 
-const Room = ({ activeTool }) => {
+type Props = {
+  activeTool: string
+};
+
+const Room = ({ activeTool }: Props) => {
   let classes = "absolute bg-blue-400 inset-0";
   if (activeTool === toolTypes.NOTE || activeTool === toolTypes.ROOM || activeTool === toolTypes.SPONGE) {
     classes += " cursor-pointer";
   }
   return <div className={classes} data-testid="Room"></div>;
-};
-
-Room.propTypes = {
-  activeTool: PropTypes.string
 };
 
 Room.defaultProps = {
