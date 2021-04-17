92141c2628bb922a55b650d0e6c23e509cf61650
Steve Cochrane <steve@stevecochrane.com>
2021-04-16 21:55:41 -0500

    Convert Room component with tests to TypeScript

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import * as React from "react";
 import toolTypes from "../constants/toolTypes";
 
 type Props = {
   activeTool: string
 };
 
-const Room = ({ activeTool }: Props) => {
+const Room: React.FC<Props> = ({ activeTool }) => {
   let classes = "absolute bg-blue-400 inset-0";
   if (activeTool === toolTypes.NOTE || activeTool === toolTypes.ROOM || activeTool === toolTypes.SPONGE) {
     classes += " cursor-pointer";
