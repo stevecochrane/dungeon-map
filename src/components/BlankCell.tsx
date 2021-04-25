ddf7fc533ac37f01987f990a128e30358e785c8c
Steve Cochrane <steve@stevecochrane.com>
2021-04-24 19:35:27 -0500

    Convert BlankCell and BlankLine to TypeScript

--- src/components/BlankCell.tsx
+++ src/components/BlankCell.tsx
@@ -1,21 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
+import * as React from "react";
 import toolTypes from "../constants/toolTypes";
 
-const BlankCell = ({ activeTool }) => {
+type Props = {
+  activeTool?: string
+};
+
+const BlankCell: React.FC<Props> = ({ activeTool }) => {
   let classes = "absolute bg-white inset-0";
   if (activeTool === toolTypes.NOTE || activeTool === toolTypes.ROOM || activeTool === toolTypes.SPONGE) {
     classes += " cursor-pointer duration-300 transition-colors hover:bg-gray-200 hover:transition-none";
   }
   return <div className={classes} data-testid="BlankCell"></div>;
-};
-
-BlankCell.propTypes = {
-  activeTool: PropTypes.string
-};
-
-BlankCell.defaultProps = {
-  activeTool: null
 };
 
 export default BlankCell;
