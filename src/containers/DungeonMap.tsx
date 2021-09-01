f1cd60381aca19fe4d0793fe6445c14639004d68
Steve Cochrane <steve@stevecochrane.com>
2021-09-01 16:42:47 -0500

    Convert DungeonMap container to TSX

--- src/containers/DungeonMap.tsx
+++ src/containers/DungeonMap.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import PropTypes from "prop-types";
+import { bindActionCreators, Dispatch } from "redux";
 import App from "../components/App";
 import { changeTool } from "../actions/tools.actions";
 import { changeMouseDown } from "../actions/mouse.actions";
 import toolTypes from "../constants/toolTypes";
 
-class DungeonMap extends React.Component {
-  constructor(props) {
+/** TODO: Separate types into their own files to be reused by reducers and actions */
+type Props = {
+  changeMouseDown: (isMouseDown: boolean) => { type: string; payload: { isMouseDown: boolean } };
+  changeTool: (toolId: string) => { type: string; payload: { tool: string } };
+  mouse: { isMouseDown: boolean };
+  tools: { activeTool: string };
+};
+
+type ReduxState = {
+  mouse: { isMouseDown: boolean };
+  tools: { activeTool: string };
+};
+
+class DungeonMap extends React.Component<Props> {
+  constructor(props: Props) {
     super(props);
     this.state = {};
     this.onToolClick = this.onToolClick.bind(this);
@@ -21,7 +33,7 @@ class DungeonMap extends React.Component {
     this.props.changeTool(toolId);
   }
 
-  handleKeyPress(event) {
+  handleKeyPress(event: KeyboardEvent) {
     let selectedTool = null;
     switch (event.code) {
       case "KeyR": {
@@ -87,21 +99,14 @@ class DungeonMap extends React.Component {
   }
 }
 
-DungeonMap.propTypes = {
-  changeMouseDown: PropTypes.func,
-  changeTool: PropTypes.func,
-  mouse: PropTypes.objectOf(PropTypes.bool),
-  tools: PropTypes.objectOf(PropTypes.string)
-};
-
-function mapStateToProps(state) {
+function mapStateToProps(state: ReduxState) {
   return {
     mouse: state.mouse,
     tools: state.tools
   };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch) {
   return bindActionCreators(
     {
       changeTool,
