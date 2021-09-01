ffa9abc160acf8003e52e08cc352f4fde2bf6f3d
Steve Cochrane <steve@stevecochrane.com>
2021-08-31 20:12:19 -0500

    Convert Tooltip to TSX, specify supported props for AutofocusTextInput

--- src/components/AutofocusTextInput.tsx
+++ src/components/AutofocusTextInput.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useRef } from "react";
 
-const AutofocusTextInput = (props: any): JSX.Element => {
+type Props = {
+  handleBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+  handleKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+};
+
+const AutofocusTextInput = ({ handleBlur, handleKeyPress }: Props): JSX.Element => {
   const inputElement = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -11,7 +16,16 @@ const AutofocusTextInput = (props: any): JSX.Element => {
     });
   }, []);
 
-  return <input type="text" data-testid="AutofocusTextInput" ref={inputElement} {...props} />;
+  return (
+    <input
+      type="text"
+      className="text-gray-800"
+      data-testid="AutofocusTextInput"
+      ref={inputElement}
+      onBlur={handleBlur}
+      onKeyPress={handleKeyPress}
+    />
+  );
 };
 
 export default AutofocusTextInput;
